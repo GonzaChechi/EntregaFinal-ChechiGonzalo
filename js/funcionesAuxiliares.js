efca19b0f8719df068c4386fe1c16c2a99041d9d
GonzaChechi <146400809+GonzaChechi@users.noreply.github.com>
2024-01-05 01:54:07 -0300

    Entrega Final

--- js/funcionesAuxiliares.js
+++ js/funcionesAuxiliares.js
@@ -2,7 +2,7 @@ let listaClientes = [];
 
 //enviamos objeto al sessionStorage
 function traerJSON() {
-    for (i = 1; i < sessionStorage.length; i++) {
+    for (i = 1; i <= sessionStorage.length; i++) {
         listaClientes.push(JSON.parse(sessionStorage.getItem("Cliente" + i)));
     }
 }
